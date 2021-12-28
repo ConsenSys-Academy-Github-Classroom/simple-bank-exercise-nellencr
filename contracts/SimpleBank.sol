0a6711c213a0691672af460d88088c6b773e87af
NELA <nellencr@gmail.com>
2021-12-28 14:50:10 +0200

    Update SimpleBank.sol

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -56,6 +56,7 @@ contract SimpleBank {
       // 1. A SPECIAL KEYWORD prevents function from editing state variables;
       //    allows function to run locally/off blockchain
       // 2. Get the balance of the sender of this transaction
+      return balances[msg.sender];
     }
 
     /// @notice Enroll a customer with the bank
@@ -63,24 +64,26 @@ contract SimpleBank {
     // Emit the appropriate event
     function enroll() public returns (bool){
       // 1. enroll of the sender of this transaction
+      enrolled[msg.sender]=true;
     }
 
-    /// @notice Deposit ether into bank
+     /// @notice Deposit ether into bank
     /// @return The balance of the user after the deposit is made
-    function deposit() public returns (uint) {
+    function deposit() public payable returns (uint) {
       // 1. Add the appropriate keyword so that this function can receive ether
     
       // 2. Users should be enrolled before they can make deposits
-
+      require(enrolled[msg.sender],"Should only enrolled to deposit");
       // 3. Add the amount to the user's balance. Hint: the amount can be
       //    accessed from of the global variable `msg`
-
+      balances[msg.sender]+= msg.value;
       // 4. Emit the appropriate event associated with this function
-
+      emit LogDepositMade(msg.sender, msg.value);
       // 5. return the balance of sndr of this transaction
+      return msg.value;
     }
 
-    /// @notice Withdraw ether from bank
+   /// @notice Withdraw ether from bank
     /// @dev This does not return any excess ether sent to it
     /// @param withdrawAmount amount you want to withdraw
     /// @return The balance remaining for the user
@@ -91,10 +94,14 @@ contract SimpleBank {
       // return the user's balance.
 
       // 1. Use a require expression to guard/ensure sender has enough funds
-
+      require(balances[msg.sender]>=withdrawAmount,"Balance insufficient to withdraw");
       // 2. Transfer Eth to the sender and decrement the withdrawal amount from
       //    sender's balance
+      balances[msg.sender]-= withdrawAmount;
 
+      msg.sender.transfer(withdrawAmount);
       // 3. Emit the appropriate event for this message
+      emit LogWithdrawal(msg.sender,balances[msg.sender],withdrawAmount);
+      return withdrawAmount;
     }
 }
