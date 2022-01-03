8590132c7396e7e91379adf2267cd187d3c2fb7b
NELA <nellencr@gmail.com>
2022-01-03 16:10:50 +0200

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,15 @@
 module.exports = {
-  compilers: {
-    solc: {
-      version: "0.6.12",
-    }
-  },
   networks: {
-    development: {
+    local: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    }
+  },
+  // To run contract with the latest compiler, uncomment lines 10-14 below:
+  compilers: { 
+    solc: {
+      version: "^0.8",    // Fetch latest 0.8.x Solidity compiler 
     }
   }
 };
